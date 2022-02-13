fa4105c84c78f4ed461ad27b790b0f54c0fc58df
ItzFelice <felicelim26@gmail.com>
2022-02-13 03:18:30 +0000

    done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,30 +2,50 @@
 	<thead>
 	  <tr>
 		<th scope="col">#</th>
-		<th scope="col">First</th>
-		<th scope="col">Last</th>
-		<th scope="col">Handle</th>
+		<th scope="col">1</th>
+		<th scope="col">2</th>
+		<th scope="col">3</th>
+		<th scope="col">4</th>
+		<th scope="col">5</th>
+		<th scope="col">6</th>
+		<th scope="col">7</th>
+		<th scope="col">8</th>
+		<th scope="col">9</th>
+		<th scope="col">10</th>
 	  </tr>
 	</thead>
 	<tbody>
 	  <tr>
-		<th scope="row">1</th>
+		<th scope="row">Mon</th>
 		<td>Mark</td>
 		<td>Otto</td>
 		<td>@mdo</td>
 	  </tr>
 	  <tr>
-		<th scope="row">2</th>
+		<th scope="row">TUES</th>
 		<td>Jacob</td>
 		<td>Thornton</td>
 		<td>@fat</td>
 	  </tr>
 	  <tr>
-		<th scope="row">3</th>
+		<th scope="row">WED</th>
 		<td colspan="2">Larry the Bird</td>
 		<td>@twitter</td>
 	  </tr>
+	  <tr>
+		<th scope="row">THURS</th>
+		<td>Jacob</td>
+		<td>Thornton</td>
+		<td>@fat</td>
+	  </tr>
+	  <tr>
+		<th scope="row">FRI</th>
+		<td>Jacob</td>
+		<td>Thornton</td>
+		<td>@fat</td>
+	  </tr>
+	  <tr>
 	</tbody>
   </table>
-  <button type="button" class="btn btn-primary btn-sm">Sign Out </button> 
+  <button type="button" class="btn btn-primary btn-sm">Log Out </button> 
  
